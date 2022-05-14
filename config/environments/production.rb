dbbdb7288597f6069c40fe36b5c92ed3b9e78f61
hassy1220 <ec2-user@ip-172-31-2-65.ap-northeast-1.compute.internal>
2022-05-14 11:56:46 +0000

    configメール関係本番環境に反映

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,6 +97,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: ENV['USERNAME'],  #Gmailアドレス（.envに記載）
+    password: ENV['PASSWORD'],  #アプリパスワード（.env）
+    authentication: :login
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
