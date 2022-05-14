5b650898da598575309ffdc177f533f393952ea4
hassy1220 <ec2-user@ip-172-31-2-65.ap-northeast-1.compute.internal>
2022-05-14 07:49:40 +0000

    GooglePlaceエラー改修

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
+// import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery";
@@ -14,23 +14,7 @@ import "../stylesheets/application";
 import '@fortawesome/fontawesome-free/js/all';
 
 Rails.start()
-Turbolinks.start()
+// Turbolinks.start()
 ActiveStorage.start()
 
-// (function($) {
-//   $(function() {
-//       var $header = $('#head_wrap');
-//       // Nav Fixed
-//       $(window).scroll(function() {
-//           if ($(window).scrollTop() > 350) {
-//               $header.addClass('fixed');
-//           } else {
-//               $header.removeClass('fixed');
-//           }
-//       });
-//       // Nav Toggle Button
-//       $('#nav-toggle, #global-nav ul li a').click(function(){
-//           $header.toggleClass('open');
-//       });
-//   });
-// });
+
