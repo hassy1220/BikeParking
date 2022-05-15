1ac539080dd0615de1b0f2e674283d8e000ca594
hassy1220 <ec2-user@ip-172-31-2-65.ap-northeast-1.compute.internal>
2022-05-15 05:02:40 +0000

    Toppe-zi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,17 +17,3 @@ Rails.start()
 // Turbolinks.start()
 ActiveStorage.start()
 
-
-// var i = 1
-// setInterval(() => {
-//     const images = document.querySelectorAll(".slider-image .top_img")
-//     i++
-//     const image = document.querySelector(".slider-image .active_img")
-//     image.classList.remove("active_img")
-//     if(i > images.length) {
-//       images[0].classList.add("active_img")
-//       i = 1
-//     } else {
-//       image.nextElementSibling.classList.add("active_img")
-//     }
-// }, 2000)
