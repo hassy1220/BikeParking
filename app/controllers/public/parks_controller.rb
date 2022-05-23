68772d20a158cc5d7fac1479d1a4284247c22d01
hassy1220 <ec2-user@ip-172-31-2-65.ap-northeast-1.compute.internal>
2022-05-23 07:52:24 +0000

    ダブルクリック

--- app/controllers/public/parks_controller.rb
+++ app/controllers/public/parks_controller.rb
@@ -97,7 +97,7 @@ class Public::ParksController < ApplicationController
 
 
   def destroy
-      if current_user.parks.exists?(id: Park.find(params[:id]))
+      if current_customer.parks.exists?(id: Park.find(params[:id]))
          @park_area = Park.find(params[:id])
          @park_area.destroy
          Park.destroy_sent_vicinity
