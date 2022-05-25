cd5cfa93c3c1e4be50fce30f801759a92fae1afe
hassy1220 <ec2-user@ip-172-31-2-65.ap-northeast-1.compute.internal>
2022-05-25 12:22:16 +0000

    skipper試作前

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ActiveStorage::AnalyzeJob.queue_adapter = :inline
-ActiveStorage::PurgeJob.queue_adapter = :inline
-Admin.create!(email:"test@test",password:111111
-  )
+# ActiveStorage::AnalyzeJob.queue_adapter = :inline
+# ActiveStorage::PurgeJob.queue_adapter = :inline
+# Admin.create!(email:"test@test",password:111111
+#   )
 
-customer = Customer.create!(
-  [
-    {email: 'yamada@test.com', name: '山田太郎', password: '111111', bike_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
-    {email: 'reiwa@test.com', name: '令和太郎', password: '111111', bike_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
-    {email: 'heisei@test.com', name: '平成太郎', password: '111111', bike_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
-  ]
-)
+# customer = Customer.create!(
+#   [
+#     {email: 'yamada@test.com', name: '山田太郎', password: '111111', bike_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")},
+#     {email: 'reiwa@test.com', name: '令和太郎', password: '111111', bike_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")},
+#     {email: 'heisei@test.com', name: '平成太郎', password: '111111', bike_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")}
+#   ]
+# )
 
 customer.each do |bike|
   if bike.id = 1
-    bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike01.jpg"), filename:"my_bike01.jpg")
-    bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike02.jpg"), filename:"my_bike02.jpg")
-    bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike03.jpg"), filename:"my_bike03.jpg")
+    # bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike01.jpg"), filename:"my_bike01.jpg")
+    # bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike02.jpg"), filename:"my_bike02.jpg")
+    # bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike03.jpg"), filename:"my_bike03.jpg")
   elsif bike.id = 2
     bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike04.jpg"), filename:"my_bike04.jpg")
     bike.my_bike_images.attach(io: File.open("#{Rails.root}/db/fixtures/my_bike05.jpg"), filename:"my_bike05.jpg")
@@ -34,112 +34,112 @@ customer.each do |bike|
   end
 end
 
-park = Park.create!(
-  [
-    {lat: 33.589766,lng: 130.402241,name: "天神中央公園駐輪場",description: "天神地下街にも近くアクセス最高です",customer_id: 1,spec: 2,price: 200, parking_time: 2,addressOutput: "福岡県福岡市中央区天神１丁目３",purpose: "天神地下街"},
-    {lat: 33.588617,lng: 130.419416,name: "博多駅前駐輪場",description: "博多駅の真前で、すぐにわかると思います！",customer_id: 2,spec: 1,price: 300, parking_time: 1,addressOutput: "福岡県福岡市博多区博多駅中央街９",purpose: "博多駅"},
-    {lat: 33.594903,lng: 130.403780,name: "博多駐輪場",description: "アンパンマンミュージアムの近くです！",customer_id: 3,spec: 3,price: 100, parking_time: 1,addressOutput: "福岡県福岡市博多区中洲５丁目６",purpose: "アンパンマンミュージアム"},
-  ]
-)
-park.each do |park|
-  if park.id == 1
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park1.jpg"), filename:"sample-park1.jpg")
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park2.jpg"), filename:"sample-park2.jpg")
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park3.jpg"), filename:"sample-park3.jpg")
-  elsif park.id == 2
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park4.jpg"), filename:"sample-park4.jpg")
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park5.jpg"), filename:"sample-park5.jpg")
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park6.jpg"), filename:"sample-park6.jpg")
-  elsif park.id == 3
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park7.jpg"), filename:"sample-park7.jpg")
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park8.jpg"), filename:"sample-park8.jpg")
-    park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park9.jpg"), filename:"sample-park9.jpg")
-  end
-end
+# park = Park.create!(
+#   [
+#     {lat: 33.589766,lng: 130.402241,name: "天神中央公園駐輪場",description: "天神地下街にも近くアクセス最高です",customer_id: 1,spec: 2,price: 200, parking_time: 2,addressOutput: "福岡県福岡市中央区天神１丁目３",purpose: "天神地下街"},
+#     {lat: 33.588617,lng: 130.419416,name: "博多駅前駐輪場",description: "博多駅の真前で、すぐにわかると思います！",customer_id: 2,spec: 1,price: 300, parking_time: 1,addressOutput: "福岡県福岡市博多区博多駅中央街９",purpose: "博多駅"},
+#     {lat: 33.594903,lng: 130.403780,name: "博多駐輪場",description: "アンパンマンミュージアムの近くです！",customer_id: 3,spec: 3,price: 100, parking_time: 1,addressOutput: "福岡県福岡市博多区中洲５丁目６",purpose: "アンパンマンミュージアム"},
+#   ]
+# )
+# park.each do |park|
+#   if park.id == 1
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park1.jpg"), filename:"sample-park1.jpg")
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park2.jpg"), filename:"sample-park2.jpg")
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park3.jpg"), filename:"sample-park3.jpg")
+#   elsif park.id == 2
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park4.jpg"), filename:"sample-park4.jpg")
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park5.jpg"), filename:"sample-park5.jpg")
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park6.jpg"), filename:"sample-park6.jpg")
+#   elsif park.id == 3
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park7.jpg"), filename:"sample-park7.jpg")
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park8.jpg"), filename:"sample-park8.jpg")
+#     park.images.attach(io: File.open("#{Rails.root}/db/fixtures/sample-park9.jpg"), filename:"sample-park9.jpg")
+#   end
+# end
 
 
 
-Vicinity.create!(
-  [
-   {vicinity_name: "23区（オンワード） 大丸福岡天神店(大きいサイズ)"},
-   {vicinity_name: "天神テルラ"},
-   {vicinity_name: "博多リバレインモール by TAKASHIMAYA"},
-   {vicinity_name: "博多辛子明太子うまか 博多阪急店"},
-   {vicinity_name: "アミュプラザ博多"},
-   {vicinity_name: "博多マルイ"},
-   {vicinity_name: "自由区（オンワード） 博多阪急"},
-   {vicinity_name: "アミュエスト博多"},
-   {vicinity_name: "23区（オンワード） 博多阪急"},
-   {vicinity_name: "KITTE博多"},
-   {vicinity_name: "和洋酒"},
-   {vicinity_name: "あき乃家"},
-   {vicinity_name: "JQ CARD エポスカウンター"},
-  ]
-)
+# Vicinity.create!(
+#   [
+#   {vicinity_name: "23区（オンワード） 大丸福岡天神店(大きいサイズ)"},
+#   {vicinity_name: "天神テルラ"},
+#   {vicinity_name: "博多リバレインモール by TAKASHIMAYA"},
+#   {vicinity_name: "博多辛子明太子うまか 博多阪急店"},
+#   {vicinity_name: "アミュプラザ博多"},
+#   {vicinity_name: "博多マルイ"},
+#   {vicinity_name: "自由区（オンワード） 博多阪急"},
+#   {vicinity_name: "アミュエスト博多"},
+#   {vicinity_name: "23区（オンワード） 博多阪急"},
+#   {vicinity_name: "KITTE博多"},
+#   {vicinity_name: "和洋酒"},
+#   {vicinity_name: "あき乃家"},
+#   {vicinity_name: "JQ CARD エポスカウンター"},
+#   ]
+# )
 
-VicinityPark.create!(
-  [
-    {vicinity_id: 1,park_id: 1},
-    {vicinity_id: 2,park_id: 1},
-    {vicinity_id: 3,park_id: 3},
-    {vicinity_id: 4,park_id: 2},
-    {vicinity_id: 5,park_id: 2},
-    {vicinity_id: 6,park_id: 2},
-    {vicinity_id: 7,park_id: 2},
-    {vicinity_id: 8,park_id: 2},
-    {vicinity_id: 9,park_id: 2},
-    {vicinity_id: 10,park_id: 2},
-    {vicinity_id: 11,park_id: 2},
-    {vicinity_id: 12,park_id: 2},
-    {vicinity_id: 13,park_id: 2},
-  ]
-)
+# VicinityPark.create!(
+#   [
+#     {vicinity_id: 1,park_id: 1},
+#     {vicinity_id: 2,park_id: 1},
+#     {vicinity_id: 3,park_id: 3},
+#     {vicinity_id: 4,park_id: 2},
+#     {vicinity_id: 5,park_id: 2},
+#     {vicinity_id: 6,park_id: 2},
+#     {vicinity_id: 7,park_id: 2},
+#     {vicinity_id: 8,park_id: 2},
+#     {vicinity_id: 9,park_id: 2},
+#     {vicinity_id: 10,park_id: 2},
+#     {vicinity_id: 11,park_id: 2},
+#     {vicinity_id: 12,park_id: 2},
+#     {vicinity_id: 13,park_id: 2},
+#   ]
+# )
 
-Comment.create!(
-  [
-    {comment: "この駐車場めっちゃ便利ですね！",customer_id: 1,park_id: 2},
-    {comment: "場所ちょっとわかりづらいですね",customer_id: 2,park_id: 2},
-    {comment: "博多駅行く時重宝してます！",customer_id: 3,park_id: 2},
-    {comment: "停めやすい！！",customer_id: 2,park_id: 2},
-    {comment: "駐車料金変わってましたよー！",customer_id: 1,park_id: 2},
-    {comment: "ここはおすすめですね！！",customer_id: 3,park_id: 2},
-    {comment: "５台くらいしか止めれないです。。。",customer_id: 2,park_id: 1},
-    {comment: "狭すぎーーーー",customer_id: 3,park_id: 1},
-    {comment: "狭いけど利便性は最高！！",customer_id: 1,park_id: 1},
-    {comment: "ここって場所わかりやすいですか？",customer_id: 2,park_id: 1},
-    {comment: "わかりやすいですよ！横通ればすぐわかると思います",customer_id: 3,park_id: 1},
-    {comment: "ありがとうがざいます！",customer_id: 2,park_id: 1},
-    {comment: "アンパンマンミュージアム行く時便利ですよ！",customer_id: 1,park_id: 3},
-    {comment: "アンパンミュージアム行かない、、、、",customer_id: 2,park_id: 3},
-    {comment: "確かに笑",customer_id: 1,park_id: 3},
-    {comment: "バイク乗りでアンパンマンミュージアム行く人いる？。。。。笑",customer_id: 3,park_id: 3},
-    {comment: "絶対おらん、、、笑",customer_id: 1,park_id: 3},
-    {comment: "ここって治安どんなですか？",customer_id: 2,park_id: 3},
-  ]
-)
+# Comment.create!(
+#   [
+#     {comment: "この駐車場めっちゃ便利ですね！",customer_id: 1,park_id: 2},
+#     {comment: "場所ちょっとわかりづらいですね",customer_id: 2,park_id: 2},
+#     {comment: "博多駅行く時重宝してます！",customer_id: 3,park_id: 2},
+#     {comment: "停めやすい！！",customer_id: 2,park_id: 2},
+#     {comment: "駐車料金変わってましたよー！",customer_id: 1,park_id: 2},
+#     {comment: "ここはおすすめですね！！",customer_id: 3,park_id: 2},
+#     {comment: "５台くらいしか止めれないです。。。",customer_id: 2,park_id: 1},
+#     {comment: "狭すぎーーーー",customer_id: 3,park_id: 1},
+#     {comment: "狭いけど利便性は最高！！",customer_id: 1,park_id: 1},
+#     {comment: "ここって場所わかりやすいですか？",customer_id: 2,park_id: 1},
+#     {comment: "わかりやすいですよ！横通ればすぐわかると思います",customer_id: 3,park_id: 1},
+#     {comment: "ありがとうがざいます！",customer_id: 2,park_id: 1},
+#     {comment: "アンパンマンミュージアム行く時便利ですよ！",customer_id: 1,park_id: 3},
+#     {comment: "アンパンミュージアム行かない、、、、",customer_id: 2,park_id: 3},
+#     {comment: "確かに笑",customer_id: 1,park_id: 3},
+#     {comment: "バイク乗りでアンパンマンミュージアム行く人いる？。。。。笑",customer_id: 3,park_id: 3},
+#     {comment: "絶対おらん、、、笑",customer_id: 1,park_id: 3},
+#     {comment: "ここって治安どんなですか？",customer_id: 2,park_id: 3},
+#   ]
+# )
 
-Favorite.create!(
-  [
-    {customer_id: 1,park_id: 2},
-    {customer_id: 2,park_id: 3},
-    {customer_id: 3,park_id: 1},
-  ]
-)
+# Favorite.create!(
+#   [
+#     {customer_id: 1,park_id: 2},
+#     {customer_id: 2,park_id: 3},
+#     {customer_id: 3,park_id: 1},
+#   ]
+# )
 
-Notification.create!(
-  [
-    {visitor_id: 3, visited_id: 1, park_id: 1, comment_id: 13, action: "comment"},
-    {visitor_id: 2, visited_id: 3, park_id: 1, comment_id: 14, action: "comment"},
-    {visitor_id: 2, visited_id: 1, park_id: 1, comment_id: 14, action: "comment"},
-    {visitor_id: 1, visited_id: 2, park_id: 3, comment_id: 15, action: "comment"},
+# Notification.create!(
+#   [
+#     {visitor_id: 3, visited_id: 1, park_id: 1, comment_id: 13, action: "comment"},
+#     {visitor_id: 2, visited_id: 3, park_id: 1, comment_id: 14, action: "comment"},
+#     {visitor_id: 2, visited_id: 1, park_id: 1, comment_id: 14, action: "comment"},
+#     {visitor_id: 1, visited_id: 2, park_id: 3, comment_id: 15, action: "comment"},
 
 
-    {visitor_id: 3, visited_id: 1, park_id: 3, comment_id: 16, action: "comment"},
-    {visitor_id: 3, visited_id: 2, park_id: 3, comment_id: 16, action: "comment"},
-    {visitor_id: 1, visited_id: 2, park_id: 3, comment_id: 17, action: "comment"},
-    {visitor_id: 1, visited_id: 3, park_id: 3, comment_id: 17, action: "comment"},
-    {visitor_id: 2, visited_id: 1, park_id: 3, comment_id: 18, action: "comment"},
-    {visitor_id: 2, visited_id: 3, park_id: 3, comment_id: 18, action: "comment"},
+#     {visitor_id: 3, visited_id: 1, park_id: 3, comment_id: 16, action: "comment"},
+#     {visitor_id: 3, visited_id: 2, park_id: 3, comment_id: 16, action: "comment"},
+#     {visitor_id: 1, visited_id: 2, park_id: 3, comment_id: 17, action: "comment"},
+#     {visitor_id: 1, visited_id: 3, park_id: 3, comment_id: 17, action: "comment"},
+#     {visitor_id: 2, visited_id: 1, park_id: 3, comment_id: 18, action: "comment"},
+#     {visitor_id: 2, visited_id: 3, park_id: 3, comment_id: 18, action: "comment"},
 
-  ]
-)
+#   ]
+# )
